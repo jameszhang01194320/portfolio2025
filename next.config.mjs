995ee894d9bde66b2f62ded4cb23fd0169edee52
jameszhang01194320 <wuab@yahoo.com>
2025-08-05 12:49:57 -0700

    更新 portfolio 源代码

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,10 @@
 const nextConfig = {
   output: 'export',
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true
-  },
-  trailingSlash: true
+  basePath: '/portfolio2025', // 这里改成你的 GitHub 仓库名
+  assetPrefix: '/portfolio2025',
 };
 
 export default nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class", // 启用 class 模式的暗色模式
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
